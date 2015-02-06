c4d176a0b86448c80af111f5a403d431cb06c609
Victor Hugo <victorvhpg@gmail.com>
2015-02-06 02:53:03 -0200

    correcao

--- assets/conteudo-posts/2015-02-06-download-de-arquivo-com-ajax/downloadB-video.js
+++ assets/conteudo-posts/2015-02-06-download-de-arquivo-com-ajax/downloadB-video.js
@@ -8,7 +8,7 @@
             video.autoplay = true;
             //cria uma URL para o video  // blob: xxxxxxx
             video.src = window.URL.createObjectURL(blob);
-            //apos o video arregar  pode remover url window.URL.revokeObjectURL(video.src);
+            //apos o video carregar  pode remover url window.URL.revokeObjectURL(video.src);
             //coloca o video no body
             document.querySelector("body").appendChild(video);
             document.querySelector("#url").innerHTML = "URL do video: <a href='" + video.src+"' target='_blank'>" + video.src+"</a>";
